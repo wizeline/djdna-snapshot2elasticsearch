4a2c049a51f3dfa58d126fcab51bf2b13b188e5b
Victor Garcia <victor.g64r@gmail.com>
2020-09-21 17:56:35 -0500

    Set default env variables

--- config/env.py
+++ config/env.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ.setdefault("ELASTICSEARCH_HOST", "localhost:9200")
-os.environ.setdefault("ELASTICSEARCH_INDEX", 'snapshot')
-os.environ.setdefault("CORENLP_HOST", "localhost:9000")
-os.environ.setdefault("LOCAL_DATA_PATH", "./tests/")
-os.environ.setdefault("LOCAL_DOWNLOAD_PATH", "./tests/")
-os.environ.setdefault("DOWJONES_APIKEY", "3facdc19fc4d1dbd8a865a25239fe836")
-os.environ.setdefault("DOWJONES_SNAPSHOTID", "3facdc19fc4d1dbd8a865a25239fe836")
+os.environ.setdefault("ELASTICSEARCH_HOST", "")
+os.environ.setdefault("ELASTICSEARCH_INDEX", "")
+os.environ.setdefault("CORENLP_HOST", "")
+os.environ.setdefault("LOCAL_DATA_PATH", "")
+os.environ.setdefault("LOCAL_DOWNLOAD_PATH", "")
+os.environ.setdefault("DOWJONES_APIKEY", "")
+os.environ.setdefault("DOWJONES_SNAPSHOTID", "")
