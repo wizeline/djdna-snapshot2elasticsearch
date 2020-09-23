dff2f5a435e0d55d75935c1b9a0ae1a6816e65e9
Victor Garcia <barcelonamane@hotmail.com>
2020-09-23 13:35:18 -0500

    Add deployment files (#7)
    * Add deployment files
    
    * Set default env variables
    
    * Apply some fixes in requirements and main

--- config/env.py
+++ config/env.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-os.environ["ELASTICSEARCH_HOST"] = ''
-os.environ["ELASTICSEARCH_INDEX"] = ''
-os.environ["CORENLP_HOST"] = ''
-os.environ["LOCAL_DATA_PATH"] = ''
-os.environ["LOCAL_DOWNLOAD_PATH"] = ''
-os.environ["DOWJONES_APIKEY"] = ''
-os.environ["DOWJONES_SNAPSHOTID"] = ''
+os.environ.setdefault("ELASTICSEARCH_HOST", "")
+os.environ.setdefault("ELASTICSEARCH_INDEX", "")
+os.environ.setdefault("CORENLP_HOST", "")
+os.environ.setdefault("LOCAL_DATA_PATH", "")
+os.environ.setdefault("LOCAL_DOWNLOAD_PATH", "")
+os.environ.setdefault("DOWJONES_APIKEY", "")
+os.environ.setdefault("DOWJONES_SNAPSHOTID", "")
